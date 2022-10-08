6e902a5324666b32137722801ca3e0921f27c4af
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-10-08 16:33:41 -0300

    carrito de compras

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "../styles/Header.scss";
 import Menu from "./Menu";
 import menu from "../assets/icons/icon_menu.svg";
 import logo from "../assets/logos/logo_yard_sale.svg";
+import AppContext from "../context/AppContext";
 import shopping from "../assets/icons/icon_shopping_cart.svg";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+
+  const { state } = useContext(AppContext);
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -45,7 +48,7 @@ const Header = () => {
           </li>
           <li className="navbar-shopping-cart">
             <img src={shopping} alt="shopping cart" />
-            <div>2</div>
+            {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
           </li>
         </ul>
       </div>
