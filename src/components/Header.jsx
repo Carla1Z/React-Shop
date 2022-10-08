e6f10f4de8d787a1e434fa460b7483f7fec1d18b
Carla1Z <carla_sabrina_zarate@hotmail.com>
2022-10-08 15:16:57 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "../styles/Header.scss";
+import Menu from "./Menu";
+import menu from "../assets/icons/icon_menu.svg";
+import logo from "../assets/logos/logo_yard_sale.svg";
+import shopping from "../assets/icons/icon_shopping_cart.svg";
+
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <nav>
+      <img src={menu} alt="menu" className="menu" />
+      <div className="navbar-left">
+        <img src={logo} alt="logo" className="logo" />
+        <ul>
+          <li>
+            <a href="/">All</a>
+          </li>
+          <li>
+            <a href="/">Clothes</a>
+          </li>
+          <li>
+            <a href="/">Electronics</a>
+          </li>
+          <li>
+            <a href="/">Furnitures</a>
+          </li>
+          <li>
+            <a href="/">Toys</a>
+          </li>
+          <li>
+            <a href="/">Others</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <ul>
+          <li className="navbar-email" onClick={handleToggle}>
+            email@example.com
+          </li>
+          <li className="navbar-shopping-cart">
+            <img src={shopping} alt="shopping cart" />
+            <div>2</div>
+          </li>
+        </ul>
+      </div>
+      {toggle && <Menu />}
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/containers/ProductList.jsx
@@ -0,0 +1,21 @@
+import useGetProducts from "../hooks/useGetProducts";
+import ProductItem from "../components/ProductItem";
+import "../styles/ProductList.scss";
+
+const API = "https://api.escuelajs.co/api/v1/products";
+
+const ProductList = () => {
+  const products = useGetProducts(API);
+
+  return (
+    <section className="main-container">
+      <div className="ProductList">
+        {products.map((product) => {
+          return <ProductItem product={product} key={product.id} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ProductList;
